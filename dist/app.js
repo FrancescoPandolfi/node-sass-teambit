d1de37b542646f97dce78b614d8d24452b534242
Francesco Pandolfi <francesco.pan@gmail.com>
2020-02-19 16:11:27 +0100

    aggiunta nav bar che appare scrollando

--- dist/app.js
+++ dist/app.js
@@ -10706,14 +10706,23 @@ return jQuery;
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 $(document).ready(function () {
-  $('.li-dropdown').click(function () {
-    console.log('hover');
-  });
   $(document).on('mouseenter', '.li-dropdown', function () {
     $(this).find('ul').addClass('dropdown');
   });
   $(document).on('mouseleave', '.li-dropdown', function () {
     $(this).find('ul').removeClass('dropdown');
+  });
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+
+    if (scroll >= 400) {
+      $('.menu-bar, nav, header').addClass('scrolled');
+    }
+
+    if (scroll < 400) {
+      $('.menu-bar, nav, header').removeClass('scrolled');
+    }
   });
 });
 
--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ var $ = require ("jquery");
 
 $(document).ready(function() {
 
-$('.li-dropdown').click(function() {
-  console.log('hover');
-});
-
 
   $(document).on('mouseenter', '.li-dropdown', function() {
     $(this).find('ul').addClass('dropdown');
@@ -13,5 +9,21 @@ $('.li-dropdown').click(function() {
   $(document).on('mouseleave', '.li-dropdown', function() {
     $(this).find('ul').removeClass('dropdown');
   });
+
+
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      console.log(scroll);
+
+      if (scroll >= 400) {
+          $('.menu-bar, nav, header').addClass('scrolled');
+      }
+      if (scroll < 400) {
+        $('.menu-bar, nav, header').removeClass('scrolled');
+      }
+  });
+
+
+
 
 });
