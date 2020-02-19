33f4f931156c83ca1db9d806fdb1ad5f4ec11798
Francesco Pandolfi <francesco.pan@gmail.com>
2020-02-19 20:36:21 +0100

    Terminata header responsive

--- dist/app.js
+++ dist/app.js
@@ -10706,22 +10706,35 @@ return jQuery;
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 $(document).ready(function () {
+  // Fa apparire il menù dropdown
   $(document).on('mouseenter', '.li-dropdown', function () {
     $(this).find('ul').addClass('dropdown');
   });
   $(document).on('mouseleave', '.li-dropdown', function () {
     $(this).find('ul').removeClass('dropdown');
   });
+  $(document).on('click', '.hamburger', function () {
+    $(this).siblings('.main-menu').toggleClass('open');
+  }); // Fa apparire la barra di navigazione dopo aver scrollato
+
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     console.log(scroll);
 
-    if (scroll >= 400) {
-      $('.menu-bar, nav, header').addClass('scrolled');
+    if (scroll >= 350) {
+      $('nav, .btn-sign').addClass('scrolled');
     }
 
-    if (scroll < 400) {
-      $('.menu-bar, nav, header').removeClass('scrolled');
+    if (scroll < 350) {
+      $('nav, .btn-sign').removeClass('scrolled');
+    }
+
+    if (scroll >= 80) {
+      $('.menu-bar, nav, header').addClass('fixed');
+    }
+
+    if (scroll < 80) {
+      $('.menu-bar, nav, header').removeClass('fixed');
     }
   });
 });
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ var $ = require ("jquery");
 
 $(document).ready(function() {
 
-
+  // Fa apparire il menù dropdown
   $(document).on('mouseenter', '.li-dropdown', function() {
     $(this).find('ul').addClass('dropdown');
   });
@@ -10,18 +10,32 @@ $(document).ready(function() {
     $(this).find('ul').removeClass('dropdown');
   });
 
+  $(document).on('click', '.hamburger', function() {
+    $(this).siblings('.main-menu').toggleClass('open');
+  });
 
+  // Fa apparire la barra di navigazione dopo aver scrollato
   $(window).scroll(function() {
       var scroll = $(window).scrollTop();
       console.log(scroll);
 
-      if (scroll >= 400) {
-          $('.menu-bar, nav, header').addClass('scrolled');
+      if (scroll >= 350) {
+          $('nav, .btn-sign').addClass('scrolled');
       }
-      if (scroll < 400) {
-        $('.menu-bar, nav, header').removeClass('scrolled');
+      if (scroll < 350) {
+        $('nav, .btn-sign').removeClass('scrolled');
+      }
+
+      if (scroll >= 80) {
+          $('.menu-bar, nav, header').addClass('fixed');
+      }
+      if (scroll < 80) {
+        $('.menu-bar, nav, header').removeClass('fixed');
       }
   });
+
+
+
 
 
 
